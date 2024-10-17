9a0414b660b13d1dcd33a2ff92f797f976bfdb23
Pavel Ivanov <mr.pavel.ivanov@gmail.com>
2024-10-18 01:05:44 +0200

    fixes

--- gi/clone.go
+++ gi/clone.go
@@ -7,6 +7,6 @@ import (
 )
 
 // Cloned returns a new iterator that consumes original iterator and clones each value.
-func Cloned[V constraints.Cloneable[V]](values iter.Seq[V]) iter.Seq[V] {
+func Cloned[V constraints.Cloneable[C], C any](values iter.Seq[V]) iter.Seq[C] {
 	return Map(values, V.Clone)
 }
