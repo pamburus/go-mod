ce385b612c95f937c3906e781b69ba47eadab10a
Pavel Ivanov <mr.pavel.ivanov@gmail.com>
2024-11-05 22:24:17 +0100

    minor refactoring

--- database/sql/sqltest/dbs/postgres/backend.go
+++ database/sql/sqltest/dbs/postgres/backend.go
@@ -1,48 +1,9 @@
 package postgres
 
 import (
-	"math/rand/v2"
-
 	"github.com/pamburus/go-mod/database/sql/sqltest/dbs/postgres/backend"
-	"github.com/pamburus/go-mod/database/sql/sqltest/dbs/postgres/backend/docker"
 )
-
-func Docker() DockerBackendBuilder {
-	return DockerBackendBuilder{}
-}
-
-// ---
-
-type DockerBackendBuilder struct {
-	options []docker.Option
-}
-
-func (b DockerBackendBuilder) WithImage(image string) DockerBackendBuilder {
-	b.options = append(b.options, docker.WithImage(image))
-
-	return b
-}
-
-func (b DockerBackendBuilder) WithRandSource(randSource rand.Source) DockerBackendBuilder {
-	b.options = append(b.options, docker.WithRandSource(randSource))
-
-	return b
-}
-
-func (b DockerBackendBuilder) New() Backend {
-	return docker.New(b.options...)
-}
-
-func (b DockerBackendBuilder) Backend() Backend {
-	return b.New()
-}
-
-// ---
 
 type Backend = backend.Backend
 
 type BackendProvider = backend.BackendProvider
-
-var (
-	_ BackendProvider = DockerBackendBuilder{}
-)
