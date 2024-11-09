b5c14e56492db6ebabf5bc08fab3ff1f6691f351
Pavel Ivanov <mr.pavel.ivanov@gmail.com>
2024-11-09 21:18:50 +0100

    build(tools): test-filter: improved test output parsing (#61)

--- build/tools/cmd/test-filter/format.go
+++ build/tools/cmd/test-filter/format.go
@@ -4,8 +4,8 @@ import "regexp"
 
 var reformatSettings = []reformatSetting{
 	{
-		pattern: regexp.MustCompile(`^(?P<result>ok|\?)?\s+(?P<module>[\w\./\-]+)\s+(?:(?P<duration>\d+\.\d+s)\s+)?(?:(?:(?P<covprefix>coverage:)\s+(?P<coverage>\d+\.\d+%)(?P<covsuffix> of statements(?: in [\w\./\-]+)?))|(?P<notice>\[no test files\]))\s*$`),
-		format:  "%-7[1]s %-79[2]s %7[3]s %[7]s%[4]s%7[5]s%[6]s",
+		pattern: regexp.MustCompile(`^(?P<result>ok|\?)?\s+(?P<module>[\w\./\-]+)\s+(?:(?P<duration>\d+\.\d+s)\s+)?(?:(?:(?P<covprefix>coverage: )\s*(?P<coverage>(?:\d+\.\d+%)|(?:\[no statements\]))(?P<covsuffix>(?: of statements(?: in [\w\./\-]+)?)|(?:\s*\[no tests to run\]))?)|(?:\s*(?P<notice>(?:\s*\[no [a-z ]+\])+)))\s*$`),
+		format:  "%-4[1]s\t%-79[2]s %7[3]s %[7]s%[4]s%6[5]s%[6]s",
 	},
 }
 
