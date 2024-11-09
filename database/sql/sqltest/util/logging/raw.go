ab823bc788d2d9b4c8c8a71b04b56a6c4da87b82
Pavel Ivanov <mr.pavel.ivanov@gmail.com>
2024-11-09 16:38:02 +0100

    added proper helper calling

--- database/sql/sqltest/util/logging/raw.go
+++ database/sql/sqltest/util/logging/raw.go
@@ -10,7 +10,7 @@ func DiscardRawLogger() RawLogger {
 }
 
 func RawPrefix(prefix string, logger RawLogger) RawLogger {
-	return &rawPrefixLogger{prefix, logger}
+	return &rawPrefixLogger{prefix, logger, help(logger)}
 }
 
 // ---
@@ -27,12 +27,39 @@ func (discardRawLogger) Logf(format string, args ...any) {}
 type rawPrefixLogger struct {
 	prefix string
 	logger RawLogger
+	helper
 }
 
 func (l *rawPrefixLogger) Log(args ...any) {
+	help(l.logger).Helper()
 	l.logger.Log(append([]any{l.prefix}, args...)...)
 }
 
 func (l *rawPrefixLogger) Logf(format string, args ...any) {
+	help(l.logger).Helper()
 	l.logger.Logf(l.prefix+" "+format, args...)
 }
+
+// ---
+
+func help(logger RawLogger) helper {
+	if h, ok := logger.(helper); ok {
+		return h
+	}
+
+	logger.Logf("warning: logger %T does not implement helper", logger)
+
+	return noHelper{}
+}
+
+// ---
+
+type helper interface {
+	Helper()
+}
+
+// ---
+
+type noHelper struct{}
+
+func (noHelper) Helper() {}
